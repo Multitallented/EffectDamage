98a9079a053c17c976b916bda93104f649d9d4ec
Multitallented <multitallented@pacbell.net>
2012-10-10 08:30:03 -0700

    updated EffectDamage

--- src/multitallented/redcastlemedia/spout/proxis/skills/EffectDamage.java
+++ src/multitallented/redcastlemedia/spout/proxis/skills/EffectDamage.java
@@ -1,11 +1,12 @@
 package multitallented.redcastlemedia.spout.proxis.skills;
 
-import multitallented.redcastlemedia.spout.proxis.models.effects.ActiveEffect;
+import java.util.logging.Level;
 import multitallented.redcastlemedia.spout.proxis.models.effects.EffectSource;
 import multitallented.redcastlemedia.spout.proxis.models.users.User;
-import org.spout.api.entity.Entity;
 import org.spout.api.entity.Player;
+import org.spout.api.geo.cuboid.Block;
 import org.spout.api.util.config.ConfigurationHolder;
+import org.spout.vanilla.component.living.VanillaEntity;
 import org.spout.vanilla.component.misc.HealthComponent;
 import org.spout.vanilla.source.DamageCause;
 
@@ -13,31 +14,34 @@ import org.spout.vanilla.source.DamageCause;
  *
  * @author Multitallented
  */
-public class EffectDamage extends EffectSource implements ActiveEffect{
+public class EffectDamage extends EffectSource {
 	public static final ConfigurationHolder DAMAGE = new ConfigurationHolder(10, "EffectDamage", "damage");
 	public static final ConfigurationHolder DAMAGE_LEVEL = new ConfigurationHolder(0, "EffectDamage", "damage-level");
 	public static final String NAME = "EffectDamage";
-
-	public EffectDamage() {
-		super(NAME);
-	}
-
-	@Override
-	public void save() {
-		//Do nothing because there is no file associated with the skillsource
-	}
-
-	@Override
-	public void load() {
-		//Do nothing because there is no file associated with the skillsource
-	}
 	
-	public void execute(String skillName, User user, Entity target){
-		Player player = getPlugin().getEngine().getPlayer(user.getName(), false);
-		try{
-			target.get(HealthComponent.class).damage(DAMAGE.getInt() + (DAMAGE_LEVEL.getInt()/* * user.getLevel() */), DamageCause.COMMAND, player, true);
-		}catch (Exception e){
-			
-		}
-	}
+    @Override
+    public void execute(User user, User target){
+        Player player = getPlugin().getEngine().getPlayer(user.getName(), false);
+        Player tPlayer = getPlugin().getEngine().getPlayer(target.getName(), false);
+        if (tPlayer.get(HealthComponent.class) == null) {
+            getPlugin().log(Level.WARNING, "[Proxis] failed to do damage to Player");
+            return;
+        }
+        tPlayer.get(HealthComponent.class).damage(DAMAGE.getInt(), DamageCause.COMMAND, player, true);
+    }
+    
+    @Override
+    public void execute(User user, VanillaEntity target) {
+        Player player = getPlugin().getEngine().getPlayer(user.getName(), false);
+        try {
+            target.getHolder().get(HealthComponent.class).damage(DAMAGE.getInt(), DamageCause.COMMAND, player, true);
+        } catch (NullPointerException npe) {
+            getPlugin().log(Level.WARNING, "[Proxis] failed to do damage to VanillaEntity");
+        }
+    }
+    
+    @Override
+    public void execute(User user, Block target) { 
+        
+    }
 }
