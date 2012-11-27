072e4f835d7c36d1ca052e774a57083bc237f135
Multitallented <daniel.miller@dmiller-PC.sgi.int>
2012-11-26 17:57:00 -0800

    Finished EffectDamage, unless something in ProxisRPG changes

--- src/multitallented/redcastlemedia/spout/proxis/skills/EffectDamage.java
+++ src/multitallented/redcastlemedia/spout/proxis/skills/EffectDamage.java
@@ -1,15 +1,16 @@
 package multitallented.redcastlemedia.spout.proxis.skills;
 
+import com.redcastlemedia.multitallented.spout.proxis.Proxis;
+import com.redcastlemedia.multitallented.spout.proxis.models.effects.EffectSource;
+import com.redcastlemedia.multitallented.spout.proxis.models.skills.Skill.CastSkill;
+import com.redcastlemedia.multitallented.spout.proxis.models.users.User;
 import java.util.HashMap;
-import multitallented.redcastlemedia.spout.proxis.managers.SkillManager;
-import multitallented.redcastlemedia.spout.proxis.managers.UserManager;
-import multitallented.redcastlemedia.spout.proxis.models.effects.EffectSource;
-import multitallented.redcastlemedia.spout.proxis.models.effects.ExpirableEffect;
-import multitallented.redcastlemedia.spout.proxis.models.users.User;
+import org.spout.api.Spout;
 import org.spout.api.entity.Entity;
 import org.spout.api.entity.Player;
-import org.spout.api.util.config.ConfigurationHolder;
-import org.spout.vanilla.component.living.VanillaEntity;
+import org.spout.api.geo.LoadOption;
+import org.spout.api.geo.cuboid.Block;
+import org.spout.api.geo.cuboid.Chunk;
 import org.spout.vanilla.component.misc.HealthComponent;
 import org.spout.vanilla.source.DamageCause;
 
@@ -17,68 +18,79 @@ import org.spout.vanilla.source.DamageCause;
  *
  * @author Multitallented
  */
-public class EffectDamage extends EffectSource implements ExpirableEffect {
-	public static final ConfigurationHolder DAMAGE = new ConfigurationHolder(10, "EffectDamage", "damage");
-	public static final ConfigurationHolder PERIOD = new ConfigurationHolder(1000, "EffectDamage", "period");
-	public static final ConfigurationHolder DURATION = new ConfigurationHolder(10000, "EffectDamage", "duration");
-	public static final ConfigurationHolder DAMAGE_INCREASE_PER_TICK = new ConfigurationHolder(1, "EffectDamage", "damage-increase-per-tick");
-	public static final ConfigurationHolder CANCEL_IF_INVULNERABLE_TARGET = new ConfigurationHolder(false, "EffectDamage", "cancel-if-invulnerable-target");
-	public static final String NAME = "EffectDamage";
-	
-	private HashMap<String, Integer> ticks = new HashMap<>();
+public class EffectDamage extends EffectSource {
+    public EffectDamage() {
+        super("EffectDamage");
+    }
 
-	public EffectDamage() {
-		super(NAME);
-	}
+    @Override
+    public void execute(Proxis plugin, CastSkill cs, User target, HashMap<String, Object> node) {
+        Player player = Spout.getEngine().getPlayer(target.NAME, true);
+        if (player == null) {
+            return;
+        }
+        damageEntity(player, cs.getCaster(), node);
+    }
 
-	@Override
-	public void save() {
-		//Do nothing because there is no file associated with the skillsource
-	}
+    @Override
+    public void execute(Proxis plugin, CastSkill cs, Entity target, HashMap<String, Object> node) {
+        damageEntity(target, cs.getCaster(), node);
+    }
 
-	@Override
-	public void load() {
-		//Do nothing because there is no file associated with the skillsource
-	}
+    @Override
+    public void execute(Proxis plugin, CastSkill cs, Block target, HashMap<String, Object> node) {
+        int radius = 1;
+        if (node.containsKey("radius")) {
+            try {
+                radius = (int) node.get("radius");
+            } catch (Exception e) {
+                
+            }
+        }
+        Chunk chunk = target.getPosition().getChunk(LoadOption.NO_LOAD);
+        if (!chunk.isLoaded()) {
+            return;
+        } 
+        for (Entity e : chunk.getEntities()) {
+            if (target.getPosition().getDistance(e.getTransform().getPosition()) > radius) {
+                continue;
+            }
+            if (e.has(HealthComponent.class) && damageCheck(cs.getCaster(), e)) {
+                damageEntity(e, cs.getCaster(), node);
+                break;
+            }
+        }
+    }
+    
+    private void damageEntity(Entity target, Player caster, HashMap<String, Object> node) {
+        if (!target.has(HealthComponent.class)) {
+            return;
+        }
+        int amount = 1;
+        if (node.containsKey("damage")) {
+            try {
+                amount = (int) node.get("damage");
+            } catch (Exception e) {
 
-	@Override
-	public long setDuration() {
-		return DURATION.getLong();
-	}
-	
-	@Override
-	public long setPeriod() {
-		return PERIOD.getLong();
-	}
+            }
+        }
+        boolean knockback = true;
+        if (node.containsKey("knockback")) {
+            try {
+                knockback = (boolean) node.get("knockback");
+            } catch (Exception e) {
 
-	@Override
-	public void applyEffect(String skillName, User user, Entity target) {
-		Player p = getPlugin().getEngine().getPlayer(user.getName(), false);
-		try {
-			ticks.put(user.getName(), 0);
-			target.get(HealthComponent.class).damage(DAMAGE.getInt(), DamageCause.COMMAND, p, true);
-		} catch (Exception e) {
-			
-		}
-	}
-
-	@Override
-	public void removeEffect(String skillName, User user, Entity target) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public void tick(Entity e) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public void execute(String skillName, User user, Entity e) {
-		try {
-			Player p = (Player) e;
-			UserManager.getUser(p.getName()).effects.add(SkillManager.getSkill(skillName).getEffect(NAME));
-		} catch (Exception ex) {
-			
-		}
-	}
+            }
+        }
+        target.get(HealthComponent.class).damage(amount, DamageCause.COMMAND, caster, knockback);
+    }
+    
+    @Override
+    public HashMap<String, Object> getDefaultNode() {
+        HashMap<String, Object> tempMap = new HashMap<>();
+        tempMap.put("knockback", true);
+        tempMap.put("damage", 1);
+        tempMap.put("radius", 1);
+        return tempMap;
+    }
 }
